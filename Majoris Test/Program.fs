1089d94b3ee818f9e7c6a70a6fae796060737506
Iksman <wesselmjr@gmail.com>
2017-05-17 11:33:24 +0200

    Condensed code

--- Majoris Test/Program.fs
+++ Majoris Test/Program.fs
@@ -1,16 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-let T = fun x y -> x
-let F = fun x y -> y
+let MULTIP = fun x y -> x * y
+let SUBT = fun x y -> x - y
+let ADD = fun x y -> x + y
+
+let TRUE = fun x y -> x
+let FALSE = fun x y -> y
 
 let TUPLE = fun x y -> fun f -> (f x) y
 
-let STUDENT = fun x -> fun f g -> f x //is actually INL   ----> Together forms Discriminated Union
+let STUDENT = fun x -> fun f g -> f x //is actually INL   ---->   Together forms Discriminated Union
 let TEACHER = fun y -> fun f g -> g y //is actually INR
 
-let FST = fun f -> (f (fun x y -> x))
-let SND = fun f -> (f (fun x y -> y))
+let FST = fun f -> (f TRUE)
+let SND = fun f -> (f FALSE)
 
 let MATCH = fun u -> fun f g -> (u f) g //Breaks the union and executes one of two ways
 
@@ -21,7 +25,7 @@ let result = MATCH (TEACHER thing2) SND FST
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" (result)
+    printfn "%A" 
     //printfn "%A" argv
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
